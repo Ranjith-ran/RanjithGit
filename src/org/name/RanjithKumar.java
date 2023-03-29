b2884695c656f8a56356fdc54f90bf6167ed1bfd
Ranjith-ran <ranjithv411@gmail.com>
2023-03-29 17:04:54 +0530

    done process

---
+++ src/org/name/RanjithKumar.java
@@ -0,0 +1,13 @@
+package org.name;
+
+public class RanjithKumar {
+	public static void main(String[] args) {
+
+		String s = "ranjith";
+		int a = 100;
+		Long l = 9514152097l;
+
+		System.out.println(s + a);
+	}
+
+}
